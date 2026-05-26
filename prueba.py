089e056763277516304ee9ba6132a50257e3e32f
David Fariña <davidfarinalema@gmail.com>
2026-05-26 18:51:58 +0200

    GUARDAR

--- prueba.py
+++ prueba.py
@@ -29,11 +29,11 @@ def validar_dni_nie(documento):
         return letra == letra_correcta
     return False
 conexion = mysql.connector.connect(
-    host="maglev.proxy.rlwy.net",
-    port=23345,
-    user="root",
-    password="mVzFQIbgGwVTqlbEhexLfCJRoCXcXHOQ",
-    database="railway"
+    host=st.secrets["MYSQL_HOST"],
+    port=st.secrets["MYSQL_PORT"],
+    user=st.secrets["MYSQL_USER"],
+    password=st.secrets["MYSQL_PASSWORD"],
+    database=st.secrets["MYSQL_DATABASE"]
 )
 cursor = conexion.cursor()
 
